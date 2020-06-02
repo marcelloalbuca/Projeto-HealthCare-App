daab426e52efd147ade82a243861841e807a3f33
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-06-02 00:12:23 -0300

    main splash runnable e startActivitu MainActivity

--- app/src/main/java/com/example/healthcare/SplashActivity.kt
+++ app/src/main/java/com/example/healthcare/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.example.healthcare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable {
+            openLoginScreen()
+        },1500)
+    }
+
+    private fun openLoginScreen() {
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
