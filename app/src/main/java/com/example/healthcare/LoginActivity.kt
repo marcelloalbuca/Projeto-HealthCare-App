d5b1dbb321c85bdafad0a2fdb6f774e707dfe41d
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-06-04 04:07:00 -0300

    Carregando Layout e utilizando controller

--- app/src/main/java/com/example/healthcare/LoginActivity.kt
+++ app/src/main/java/com/example/healthcare/LoginActivity.kt
@@ -2,11 +2,38 @@ package com.example.healthcare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginPresenter.View {
+
+    private lateinit var presenter: LoginPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        presenter = LoginPresenter(this)
+
+        val buttonLogin = findViewById<Button>(R.id.button_login)
+
+        buttonLogin.setOnClickListener(){
+            presenter.onLoginClicked()
+        }
     }
+
+    override fun getUserName(): String {
+        val userName = findViewById<EditText>(R.id.edit_user_name)
+        return userName.text.toString()
+    }
+    override fun getUserPassword(): String {
+        val userPassword = findViewById<EditText>(R.id.edit_user_password)
+        return userPassword.text.toString()
+    }
+
+    override fun showEmptyInformationWarnning() {
+        Toast.makeText(this, "Nome e Senha vazios", Toast.LENGTH_LONG).show()
+    }
+
 }
