815481c1d65729113126e8a6d854a4f4fbfc945f
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-06-05 21:37:46 -0300

    WifiManager

--- app/src/main/java/com/example/healthcare/WifiController.kt
+++ app/src/main/java/com/example/healthcare/WifiController.kt
@@ -1,4 +1,15 @@
 package com.example.healthcare
 
-class WifiController {
+import android.content.Context
+import android.net.wifi.WifiManager
+import com.example.healthcare.IWifiController
+
+class WifiController
+    (
+    private val context: Context
+) : IWifiController {
+    override fun isWifiEnabled(): Boolean {
+       val wifiManager: WifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        return wifiManager.isWifiEnabled
+    }
 }
