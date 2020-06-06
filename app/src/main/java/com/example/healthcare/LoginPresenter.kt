43d53451fa306c7d59520ba170b358f47aef6be7
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-06-05 21:44:48 -0300

    add interfaces e metodos

--- app/src/main/java/com/example/healthcare/LoginPresenter.kt
+++ app/src/main/java/com/example/healthcare/LoginPresenter.kt
@@ -1,12 +1,19 @@
 package com.example.healthcare
 
+
 class LoginPresenter (
-    private val view: View
+    private val view: View,
+    private val wifiController: IWifiController,
+    private val loginController: ILoginController
 )   {
     interface View {
         fun getUserName(): String
         fun getUserPassword(): String
         fun showEmptyInformationWarnning()
+        fun showWifiDisabledWarning()
+        fun showUnauthentedUser()
+        fun openHomeScreen()
+
     }
 
     fun onLoginClicked(){
@@ -15,6 +22,20 @@ class LoginPresenter (
 
         if (userName.isEmpty() || userPassword.isEmpty()) {
             view.showEmptyInformationWarnning()
+            return;
         }
+
+        if (!wifiController.isWifiEnabled()) {
+            view.showWifiDisabledWarning()
+            return
+        }
+
+        loginController.login(userName, userPassword, callback = { isUserLogged ->
+            if (isUserLogged) {
+                view.openHomeScreen()
+            }else{
+                view.showUnauthentedUser()
+            }
+        })
     }
 }
